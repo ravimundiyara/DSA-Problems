5a80a4bfcf95664e853c7fc7006e94e1cff7a74b
Ravi Mundiyara <ravimundiyara4@gmail.com>
2026-09-06 10:00:21 +0530

    Time: 0 ms (100%), Space: 14.2 MB (58.16%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,41 +1,24 @@
 class Solution {
 public:
-
-    void solve(vector<int>& candidates, int target, int index,
-               vector<int>& temp, vector<vector<int>>& ans) {
-
-        // Target reached
-        if (target == 0) {
-            ans.push_back(temp);
+    void findcombination(int ind, int target, vector<int>& arr, vector<vector<int>>&ans, vector<int>&ds){
+        if(ind==arr.size()){
+            if(target==0){
+                ans.push_back(ds);
+            }
             return;
         }
-
-        // No more candidates or target becomes negative
-        if (index == candidates.size() || target < 0) {
-            return;
+        if(arr[ind]<=target){
+            ds.push_back(arr[ind]);
+            findcombination(ind, target-arr[ind], arr, ans,ds);
+            ds.pop_back();
         }
-
-        // Choice 1: Take current element
-        temp.push_back(candidates[index]);
-
-        // Stay at same index because we can reuse the element
-        solve(candidates, target - candidates[index],
-              index, temp, ans);
-
-        temp.pop_back();
-
-        // Choice 2: Don't take current element
-        solve(candidates, target, index + 1,
-              temp, ans);
+        findcombination(ind+1, target, arr, ans, ds);
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-
-        vector<vector<int>> ans;
-        vector<int> temp;
-
-        solve(candidates, target, 0, temp, ans);
-
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findcombination(0, target, candidates, ans, ds);
         return ans;
     }
 };
