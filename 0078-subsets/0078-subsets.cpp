46a7b2f448a21c45661a78b6e7304797bf251483
Ravi Mundiyara <ravimundiyara4@gmail.com>
2026-09-08 16:16:30 +0530

    Time: 0 ms (100%), Space: 15.7 MB (7.38%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void findsubsets(int ind, vector<int>&nums, vector<vector<int>>&ans, vector<int>ds){
+        if(ind==nums.size()){
+            ans.push_back(ds);
+            return;
+        }
+
+        ds.push_back(nums[ind]);
+        findsubsets(ind+1, nums, ans, ds);//take case
+        ds.pop_back();
+        findsubsets(ind+1, nums, ans, ds);//don't take case
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>ans;
+        vector<int>ds;
+        findsubsets(0, nums, ans, ds);
+        return ans;
+    }
+};
